94dcd372e37f64d9b738109de4f2294512d0c82d
FRDEEN KHAN <khanfardeen688@gmail.com>
2023-12-31 12:44:54 +0300

    services update for tausif

--- content/models.py
+++ content/models.py
@@ -6,12 +6,16 @@ class BannerContent(models.Model):
     banner_contnet_english = models.TextField()
     banner_header_arabic = models.CharField(max_length=625)
     banner_contnet_arabic = models.TextField()
+    def __str__(self):
+        return str(self.banner_header_english)
 
 class Content(models.Model):
     story_header_english = models.CharField(max_length=625)
     story_contnet_english = models.TextField()
     story_header_arabic = models.CharField(max_length=625)
     story_contnet_arabic = models.TextField()
+    def __str__(self):
+        return str(self.story_header_english)
 
 # class Pricing(models.Model):
 #     started_price = models.IntegerField(default=0)
@@ -24,6 +28,8 @@ class Faq(models.Model):
     answer = models.TextField()
     title_arabic = models.CharField(max_length=255)
     answer_arabic = models.TextField()
+    def __str__(self):
+        return str(self.title)
 
 class AboutUs(models.Model):
     text = models.TextField()
@@ -34,6 +40,8 @@ class AboutUs(models.Model):
     location_arabic = models.CharField(max_length=100)
     contact_no_arabic = models.CharField(max_length=13)
     email_arabic = models.EmailField()
+    def __str__(self):
+        return str(self.contact_no)
 
 class SocialMedia(models.Model):
     instagram_link = models.URLField()
